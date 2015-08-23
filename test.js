5ef0e107e81ee11fc5ae47cca3e5f7dcc5fce93c
pemrouz <p3pedram@hotmail.com>
2015-08-23 12:33:19 +0100

    test all cases

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect
 
 describe('az', function() {
   it('should filter less than value', function() {
-    var a = [{ val: 3 }, { val: 1 }, { val: 2 }]
-    expect(a.sort(az('val'))).to.be.eql([{ val: 1 }, { val: 2 }, { val: 3 }])
+    var a = [{ val: 2 }, { val: 3 }, { val: 1 }, { val: 2 }]
+    expect(a.sort(az('val'))).to.be.eql([{ val: 1 }, { val: 2 }, { val: 2 }, { val: 3 }])
   })
 })
